06ac4f4df55cb1548a46ad2c5f003d3da43d1206
ewhite20 <62667019+ewhite20@users.noreply.github.com>
2021-11-21 22:11:58 -0500

    Update cryptoTest.java

--- cryptoTest.java
+++ cryptoTest.java
@@ -7,14 +7,22 @@ import org.junit.jupiter.api.Test;
 class cryptoTest {
 
     @Test
-    public void testRSA() {
+    public void testDecode() {
         assertEquals(2, crypto.decode(33, 7, 29));
         int a= crypto.decode(133, 29, 92);
         assertEquals(99, a);
+    }
+
+    @Test
+    public void testFind() {
         assertEquals("c", crypto.find(13, 0));
         assertEquals("ab", crypto.find(1112, 0));
         assertEquals("abcde", crypto.find(1112131415, 0));
         assertEquals("abcde", crypto.find(1213141516, 1));
+    }
+
+    @Test
+    public void testEnDecode() {
         int b= crypto.encode(2024, 212141, 487001);
         assertEquals(480272, b);
         assertEquals(2024, crypto.decode(487001, 212141, b));
